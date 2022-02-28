c31ee3b9b19bb25f2e584106525b9b2413fbb52f
sean kim <ddhyun93@gmail.com>
2022-02-28 16:49:25 +0900

    EDIT gitignore

---
+++ const/constlocal.go
@@ -0,0 +1,28 @@
+package _const
+
+const (
+	//NCPKEYID = "mq9biarq8p"
+	//NCPKEY   = "dhYLDFt88KIiuehGzNUsb4mnEuoyfyQywEvJH8hU"
+	//NCPKEYID = "o2h1p3y2g3"
+	//NCPKEY   = "XQwNE3H0FW9oR0tfkhVdiCfuTVWuZfX2RNo1fwOX"
+	NCPKEYID = "2p449y77e5"
+	NCPKEY   = "vehE0ooYWMXbXgamHvs0NFJZM5GEswMHgX8s6ujM"
+
+	//dev
+	//MYSQLUSER = "msg_dev"
+	//MYSQLPASS = "msgDev2021;"
+	//MYSQLHOST = "msg-dev.czvkrw2zkctv.ap-northeast-2.rds.amazonaws.com"
+	//MYSQLDB   = "msg_dev"
+
+	// local
+	MYSQLUSER = "root"
+	MYSQLPASS = ""
+	MYSQLHOST = "127.0.0.1"
+	MYSQLDB   = "test"
+
+	// prod
+	//MYSQLUSER = "twinkorea_prod"
+	//MYSQLPASS = "Twin202!Korea"
+	//MYSQLHOST = "twinkorea-prod.czvkrw2zkctv.ap-northeast-2.rds.amazonaws.com"
+	//MYSQLDB   = "twinkorea_prod"
+)
